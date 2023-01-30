3d0a57adcfe093a858b1e78385561bc46a7e1f9c
Eya ben attig <eyabenattig@gmail.com>
2023-01-30 13:43:36 +0100

    add crudapi

---
+++ client/src/components/InputsGroup.js
@@ -0,0 +1,23 @@
+
+import {
+    FormControl,
+    FormErrorMessage,
+    FormLabel,
+    Input,
+  } from '@chakra-ui/react';
+
+const InputsGroup = ({ name, onChangeHandler, value, errors }) => {
+    
+      return (
+    <FormControl isInvalid={errors}>
+    <FormLabel>{name}</FormLabel>
+    <Input type="text" name={name} onChange={onChangeHandler} value={value} />
+    {errors &&
+      errors?.map((err) => {
+        return <FormErrorMessage>{err}</FormErrorMessage>;
+      })}
+  </FormControl>
+  )
+}
+
+export default InputsGroup
